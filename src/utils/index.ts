e35196a6bc09e594d665402ef3a07973d792144b
vijay kodibailu <vijaykodibailu@yahoo.in>
2021-12-20 02:56:08 +0530

    home page loads shopify data & display products

---
+++ src/utils/index.ts
@@ -0,0 +1,51 @@
+    if (process.env.NEXT_PUBLIC_API_URL === undefined) {
+        throw new Error(`Missing API URL`);
+      } 
+    
+    
+    if (process.env.NEXT_PUBLIC_ACCESS_TOKEN === undefined) {
+        throw new Error(`Missing Access Token`);
+      } 
+
+export async function storefront(query: any) {
+    if (process.env.NEXT_PUBLIC_API_URL === undefined) {
+        throw new Error(`Missing API URL`);
+      }
+      
+      if (process.env.NEXT_PUBLIC_ACCESS_TOKEN === undefined) {
+        throw new Error(`Missing Access Token`);
+      } 
+    const URL = process.env.NEXT_PUBLIC_API_URL
+
+    const options = {
+      endpoint: URL,
+      method: "POST",
+      headers: {
+        "X-Shopify-Storefront-Access-Token": process.env.NEXT_PUBLIC_ACCESS_TOKEN,
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ query })
+    }
+  
+    try {
+      const data = await fetch(URL, options).then(response => {
+        return response.json()
+      })
+    //   console.log('data server', data)
+  
+      return data
+    } catch (error) {
+      throw new Error("Products not fetched")
+    }
+  }
+
+
+  export function formatePrice(number: number | bigint) {
+      return Intl.NumberFormat('en-AU', {
+          style: "currency", 
+          currency:"AUD", 
+          minimumFractionDigits:0
+        }
+    ).format(number)
+  }
